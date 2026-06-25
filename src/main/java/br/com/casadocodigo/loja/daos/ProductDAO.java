60e461aaf82b54cc28dd9f1fb1784ae9b99b3ebd
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-06-25 19:52:03 -0300

    Disponibilizando objetos na view e listando produtos

--- src/main/java/br/com/casadocodigo/loja/daos/ProductDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProductDAO.java
@@ -1,20 +1,28 @@
 package br.com.casadocodigo.loja.daos;
 
-import org.springframework.stereotype.Repository;
-
-import br.com.casadocodigo.loja.models.Product;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.models.Product;
 
 @Repository
 public class ProductDAO {
-	
+
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	public void save(Product product) {
 		manager.persist(product);
+	}
+
+	@RequestMapping("/produtos")
+	public ModelAndView list() {
+		ModelAndView modelAndView = new ModelAndView("products/list");
+		modelAndView.addObject("products", productDAO.list());
+		return modelAndView;
 	}
 }
