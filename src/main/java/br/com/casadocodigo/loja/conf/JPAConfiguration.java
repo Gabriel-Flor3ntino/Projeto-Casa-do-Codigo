8704ef2d467c79c3cff5700b2ea3b6dc1b67d2bf
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-07-10 21:13:53 -0300

    Utilizando Profiles e controlando ambiente

--- src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -1,14 +1,13 @@
 package br.com.casadocodigo.loja.conf;
 
-import java.lang.management.PlatformLoggingMXBean;
 import java.util.Properties;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -36,6 +35,7 @@ public class JPAConfiguration {
 	}
 
 	@Bean
+	@Profile("dev")
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -1,33 +1,41 @@
 package br.com.casadocodigo.loja.conf;
 
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[]{SecurityConfiguration.class, 
-				AppWebConfiguration.class,
-				JPAConfiguration.class,
-				AmazonConfiguration.class};
+		return new Class[] { SecurityConfiguration.class, AppWebConfiguration.class, JPAConfiguration.class,
+				AmazonConfiguration.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		// Tem que colocar aqui para ser adicionado no carregamento da servlet base
-		return new Class[]{};
+		return new Class[] {};
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		return new String[] {"/"};
+		return new String[] { "/" };
 	}
-	
+
 	@Override
 	protected void customizeRegistration(Dynamic registration) {
 		registration.setMultipartConfig(new MultipartConfigElement(""));
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		servletContext.addListener(RequestContextListener.class);
+		servletContext.setInitParameter("spring.profiles.active", "dev");
 	}
 }
